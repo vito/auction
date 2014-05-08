07b7c3b6abc1518efe996433855cbfe7f6fa4d21
Onsi Fakhouri <onsi@pivotallabs.com>
2014-05-08 11:54:54 -0700

    refactor lossy rep

--- lossyrep/rep.go
+++ lossyrep/rep.go
@@ -15,7 +15,6 @@ var Timeout time.Duration
 
 var Flakiness = 1.0
 
-var TimeoutError = errors.New("timedout")
 var FlakyError = errors.New("flakeout")
 
 type LossyRep struct {
@@ -30,56 +29,44 @@ func New(totalResources int, flaky bool, instances map[string]instance.Instance)
 	}
 }
 
-func (rep *LossyRep) Vote(instance instance.Instance) (float64, error) {
+func (rep *LossyRep) beSlowAndFlakey() bool {
 	if rep.Flaky {
 		if util.Flake(Flakiness) {
 			time.Sleep(Timeout)
-			return 0, FlakyError
+			return true
 		}
 	}
 	ok := util.RandomSleep(LatencyMin, LatencyMax, Timeout)
 	if !ok {
-		return 0, TimeoutError
+		return true
 	}
 
+	return false
+}
+
+func (rep *LossyRep) Vote(instance instance.Instance) (float64, error) {
+	if rep.beSlowAndFlakey() {
+		return 0, FlakyError
+	}
 	return rep.Rep.Vote(instance)
 }
 
 func (rep *LossyRep) ReserveAndRecastVote(instance instance.Instance) (float64, error) {
-	if rep.Flaky {
-		if util.Flake(Flakiness) {
-			time.Sleep(Timeout)
-			return 0, FlakyError
-		}
-	}
-	ok := util.RandomSleep(LatencyMin, LatencyMax, Timeout)
-	if !ok {
-		return 0, TimeoutError
+	if rep.beSlowAndFlakey() {
+		return 0, FlakyError
 	}
 
 	return rep.Rep.ReserveAndRecastVote(instance)
 }
 
 func (rep *LossyRep) Release(instance instance.Instance) {
-	if rep.Flaky {
-		if util.Flake(Flakiness) {
-			time.Sleep(Timeout)
-			return
-		}
-	}
-	util.RandomSleep(LatencyMin, LatencyMax, Timeout)
+	rep.beSlowAndFlakey()
 
 	rep.Rep.Release(instance)
 }
 
 func (rep *LossyRep) Claim(instance instance.Instance) {
-	if rep.Flaky {
-		if util.Flake(Flakiness) {
-			time.Sleep(Timeout)
-			return
-		}
-	}
-	util.RandomSleep(LatencyMin, LatencyMax, Timeout)
+	rep.beSlowAndFlakey()
 
 	rep.Rep.Claim(instance)
 }
